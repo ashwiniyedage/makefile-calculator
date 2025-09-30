6deadc8ccb03a8b9d48213f2ad4bc87b44419e4b
Ashwini <yedgeashwini@gmail.com>
2025-09-30 13:30:26 +0530

    Initial commit

---
+++ cal.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include "functions.h"
+
+using namespace std;
+
+int main()
+{
+    int ch;
+    do{
+        cout<<"1.Addition \n2.Substraction \n3.Exit \nEnter your choice : "<<endl;
+        cin>>ch;
+
+        switch(ch)
+        {
+            case 1: add();
+                    break;
+            case 2: sub();
+                    break;
+            case 3: break;
+            default : 
+                cout<<"Invalid input. plz try again"<<endl;
+                break;
+        }
+    }while(ch!=0);
+}
